7f4f57f39eaa8b65cf9492e7908fc2bfc8def82a
Gunalan S <34304620+s-gunalan@users.noreply.github.com>
2018-06-12 08:41:34 +0530

    Update mainJs.js

--- javascript/mainJs.js
+++ javascript/mainJs.js
@@ -149,7 +149,7 @@ function queryDpBot(action,text) {
             insertChat("remote", "Sorry ULTRON has faced some issues! Please try again later");
         }
     });
-}
+}}
 
 function generateUUID() { // Public Domain/MIT
     var d = new Date().getTime();
