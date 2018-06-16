58830ea470149862e079d89a6e46fdb3b00a5247
Gunalan S <34304620+s-gunalan@users.noreply.github.com>
2018-06-16 19:30:17 +0530

    Update mainJs.js

--- javascript/mainJs.js
+++ javascript/mainJs.js
@@ -18,7 +18,47 @@ function formatTime(date) {
     var strTime = hours + ':' + minutes + ' ' + ampm;
     return strTime;
 }
-
+$("#rec").click(function(event) {
+            switchRecognition();
+        });
+    });
+    var recognition;
+    function startRecognition() {
+        recognition = new webkitSpeechRecognition();
+        recognition.onstart = function(event) {
+            updateRec();
+        };
+        recognition.onresult = function(event) {
+            var text = "";
+            for (var i = event.resultIndex; i < event.results.length; ++i) {
+                text += event.results[i][0].transcript;
+            }
+            setInput(text);
+            stopRecognition();
+        };
+        recognition.onend = function() {
+            stopRecognition();
+        };
+        recognition.lang = "en-US";
+        recognition.start();
+    }
+    function stopRecognition() {
+        if (recognition) {
+            recognition.stop();
+            recognition = null;
+        }
+        updateRec();
+    }
+    function switchRecognition() {
+        if (recognition) {
+            stopRecognition();
+        } else {
+            startRecognition();
+        }
+    }
+function updateRec() {
+        $("#rec").text(recognition ? "Stop" : "Speak");
+    }
 function insertChat(who, text) {
     var control = "";
     var date = formatTime(new Date());
